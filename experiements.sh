d0c2216f6d078f5f7e1e6eb2d1c6958a42ab9c3f
NueveCuatro <coleelliot2001@gmail.com>
2025-10-08 14:28:41 +0200

    back up

---
+++ experiements.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+# This file containes the experiments
+
+python train.py \
+       --name gpt2_openwebtext_100k_ptl_1_4_7_luni_05_lid_1  \
+       --model_name_or_path gpt2 \
+       --dataset_name openwebtext_tokkenized_1024  \
+       --text_column text \
+       --model causallm \
+       --dataset_mode causallm \
+       --torch_dtype 32 \
+       --n_epochs 1 \
+       --batch_size 4 \
+       --lr 2e-5 \
+       --weight_decay 0.33 \
+       --optimizer AdamW \
+       --lr_policy linear \
+       --freeze_all \
+       --max_samples 100000 \
+       --warmup_steps 500  \
+       --display_freq 100 \
+       --save_model_freq 10000 \
+       --wm passthrough \
+       --num_data_workers 5 \
+       --wm_key 8888 \
+       --wm_seed 42 \
+       --lambda_id 1 \
+       --lambda_uni 0.5 \
+       --ptl_idx 1 4 7 \
+       --use_wandb \
